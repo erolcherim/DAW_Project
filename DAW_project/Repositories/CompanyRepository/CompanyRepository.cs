f98bad395cfcbd58989378a50442cbff7c2cc33e
erolcherim <erol.cherim@gmail.com>
2022-08-28 16:20:40 +0300

    Added company repository

---
+++ DAW_project/Repositories/CompanyRepository/CompanyRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAW_project.DAL;
+using DAW_project.DAL.Models;
+using DAW_Project.Repositories.GenericRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAW_Project.Repositories.CompanyRepository
+{
+    public class CompanyRepository : GenericRepository<Company>, ICompanyRepository
+    {
+        public CompanyRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<List<Company>> GetAllCompaniesAsync() =>
+            await _context.Companies.ToListAsync();
+
+        public async Task<Company> GetCompanyByNameAsync(string name) =>
+            await _context.Companies.Where(c => c.CompanyName.Equals(name)).FirstOrDefaultAsync();
+
+        public async Task<Company> GetCompanyByCEO(string ceo) =>
+            await _context.Companies.Where(c => c.CEO.Equals(ceo)).FirstOrDefaultAsync();
+
+    }
+}
---
+++ DAW_project/Repositories/CompanyRepository/ICompanyRepository.cs
@@ -0,0 +1,12 @@
+﻿using DAW_project.DAL.Models;
+using DAW_Project.Repositories.GenericRepository;
+
+namespace DAW_Project.Repositories.CompanyRepository
+{
+    public interface ICompanyRepository : IGenericRepository<Company>
+    {
+        Task<List<Company>> GetAllCompaniesAsync();
+        Task<Company> GetCompanyByNameAsync(string name);
+        Task<Company> GetCompanyByCEO(string ceo);
+    }
+}
